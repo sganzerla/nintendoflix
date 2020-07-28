7efe807c18f8f3028ed201e93b63d761c4f3950e
Alisson Michel Sganzerla <alisson_sganzerla@yahoo.com.br>
2020-07-28 16:38:34 -0300

    initial form

--- aluraflix/src/pages/cadastro/Categoria/index.js
+++ aluraflix/src/pages/cadastro/Categoria/index.js
@@ -5,7 +5,15 @@ import PageDefault from '../../../components/PageDefault';
 function CadastroCategoria() {
     return (
         <PageDefault>
-            <h1>Cadastro de vídeo</h1>
+            <h1>Cadastro de categoria</h1>
+            <form>
+                <label>
+                    Nome da Categoria:
+                    <input type="text" />
+                </label>
+                <button>Cadastrar</button>
+            </form>
+
             <Link to="/">
                 Ir para home
             </Link>
