6e541bd0671bfa9167f7dfb3ca6168ef4415bc88
alisson sganzerla <alisson_sganzerla@yahoo.com.br>
2020-07-27 23:32:55 -0300

    styled component

--- aluraflix/src/components/Menu/index.js
+++ aluraflix/src/components/Menu/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
+import Button from '../Button';
 
 function Menu() {
 
     return (
         <nav className="Menu">
             <a href="/">
-               <img className="Logo" src={Logo} alt="Aluraflix logo"/>
+                <img className="Logo" src={Logo} alt="Aluraflix logo" />
             </a>
-            <a className="ButtonLink" href="/"> Novo vídeo</a>
+            <Button as="a" className="ButtonLink" href="/">
+                Novo vídeo
+             </Button>
         </nav>
     );
 
